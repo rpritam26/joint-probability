04eece655d43fa9d88c27b3388f76400245b341b
rpritam26 <157622320+rpritam26@users.noreply.github.com>
2024-09-29 10:05:09 +0530

    Create binomial.py

---
+++ binomial.py
@@ -0,0 +1,9 @@
+#normal distribution
+import numpy as np 
+import scipy as sp 
+from scipy import stats 
+import matplotlib.pyplot as plt  
+x_data = np.arange(0, 2000,0.01) 
+y_data = stats.norm.pdf(x_data, 1100,300) 
+plt.figure(figsize=(6, 4))
+plt.plot(x_data, y_data)
